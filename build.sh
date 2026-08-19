a52b436c96113380b57e75ab837ceabbf22bd02e
Saleh Rafi <rafi221112@gmail.com>
2026-08-19 14:34:34 +0300

    Update build.sh comments for clarity

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
+# Clean up existing manifests on the dev node
 rm -rf .repo/local_manifests .repo/manifests
 
-# Initializes AOSP 15.0 manifest from scratch
+# Initialize your AOSP 15.0 manifest tag or your main branch
 repo init -u https://github.com/Rverseking/EternalOS-Manifests.git -b main --depth=1
 
-# Sync the source code using all available CPU threads
+# Sync the AOSP 15 source code
 repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc)
-
